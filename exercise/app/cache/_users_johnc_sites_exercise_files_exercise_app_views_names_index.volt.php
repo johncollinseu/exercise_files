d420b5eba1f2973648a97dc89257f4e1dfbea8ee
John Collins <john@johncollins.eu>
2014-11-25 12:06:55 +0000

    Update .gitignore

--- exercise/app/cache/_users_johnc_sites_exercise_files_exercise_app_views_names_index.volt.php
+++ exercise/app/cache/_users_johnc_sites_exercise_files_exercise_app_views_names_index.volt.php
@@ -2,6 +2,3 @@
 <p>Hello your name is <strong><?php echo $name->name; ?></strong></p>
 
 <p><?php echo $this->tag->linkTo(array('index/', 'Back to home page')); ?></p>
-
-
-<p><?php echo $this->tag->linkTo(array('names/index/', 'Back to home page')); ?></p>
